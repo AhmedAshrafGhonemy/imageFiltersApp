c93c3fca3ddd62f42cdee3eb4de7f017bb563162
AhmedAshrafghonemy <AhmedAshrafghonemy@gmail.com>
2021-12-22 16:18:51 +0200

    Initial Commit

---
+++ ImageFilter/ImageFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFilterApp.swift
+//  ImageFilter
+//
+//  Created by macbook on 22/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct ImageFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
